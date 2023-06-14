dfaa14bd9f2aab3c5089b530865fb0ffd96eb37f
Davi menezes <77814500+Lukuko@users.noreply.github.com>
2023-06-14 09:59:00 -0300

    Update Index.js

--- src/Main/Index.js
+++ src/Main/Index.js
@@ -1,17 +1,10 @@
-import AddButton from "../components/AddButton/Index"
 import Header from "../components/Header/Index";
 import ProdutosScreen from "../components/Products/Index";
 import { StatusBar } from "expo-status-bar";
 import { Container } from "./Style";
-import CustomModal from '../components/CustomModal/Index'
-import { useState } from "react";
-import DescriptionButton from "../components/DescriptionButton/Index";
 import SearchBar from "../components/SearchBar/Index";
 
 export default function Main() {
-  const [isModalVisible, setIsModalVisible] = useState(false);
-  console.log("o estado do botão é",isModalVisible)
-
   return (
     <Container>        
       <StatusBar
@@ -21,9 +14,6 @@ export default function Main() {
       <Header />
       <SearchBar />
       <ProdutosScreen />
-      <CustomModal 
-        isVisible={isModalVisible}
-        onClose={() => {setIsModalVisible(!isModalVisible);}}/>
     </Container>
   )
 }
